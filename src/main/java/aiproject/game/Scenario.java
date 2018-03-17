7a20c5107a08fe34b240cd469dc80bab5d5ca4a5
BradleyWood <bradley.wood@uoit.net>
2018-03-17 00:21:33 -0400

    added scenario numbers to enum

--- src/main/java/aiproject/game/Scenario.java
+++ src/main/java/aiproject/game/Scenario.java
@@ -1,7 +1,17 @@
 package aiproject.game;
 
 public enum Scenario {
-    COMPETITION,
-    COLLABORATION,
-    COMPASSION
+    COMPETITION(1),
+    COLLABORATION(2),
+    COMPASSION(3);
+
+    private final int num;
+
+    Scenario(final int num) {
+        this.num = num;
+    }
+
+    public int getNum() {
+        return num;
+    }
 }
